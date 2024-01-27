8eded68bfea74d28f6aebf8f9216737e31ee744c
Gabriel <127830805+pescamolle@users.noreply.github.com>
2024-01-27 18:31:10 +0100

    Update check_battery.py

--- check_battery_status/check_battery.py
+++ check_battery_status/check_battery.py
@@ -1,4 +1,5 @@
 #A simple program that allows you to check your battery percentage
+
 from win11toast import toast
 import psutil as ps
 
@@ -13,37 +14,26 @@ low_battery_icon = {
 }
 
 mid_battery_icon = {
-    'src' : 'mid_charged.ico',
+    'src' : 'C:\\Users\\gabri\\OneDrive\\Desktop\\Zona mij\\Python\\Check_Battery\\mid_charged.ico',
     'placement': 'appLogoOverride'
 }
 
-battery = ps.sensors_battery()
-battery_percentage = battery.percent
-AC_power = battery.power_plugged
+
+loopToken = True #To not receive infinite notifications :p
 
 
 #This block checks when you should plug/unplug your AC adapter automatically
-if (battery_percentage <= 45) and (AC_power == False):
-    toast(title='Battery about %s%%' % (battery_percentage),
-          body='Battery low, please put in charge your PC',icon=low_battery_icon)
-elif (battery_percentage >=60) and (AC_power == True):
-    toast(title='Battery about %s%%' % (battery_percentage),
-          icon=mid_battery_icon)
-elif (battery_percentage >= 90) and (AC_power == True):
-    toast(title='Battery about %s%%' % (battery_percentage),
-          body='Battery charged, please remove the power cable', 
-          icon=charged_battery_icon)
-
-#This block works only if you execute the program
-if (battery_percentage > 75) and (AC_power == False):
-    toast(title='Battery about %s%%' % (battery_percentage),
-          body='No need to charge at the moment',
-          icon=charged_battery_icon)
-elif ((battery_percentage > 45) and (battery_percentage < 75)) and (AC_power == False):
-    toast(title='Battery about %s%%' % (battery_percentage),
-          body='No need to charge at the moment',
-          icon=mid_battery_icon)
-elif AC_power == True:
-        toast(app_id='Battery status',title='Battery about %s%%' % (battery_percentage),
-          body='Currently charging',
-          icon=mid_battery_icon)
+while True:
+    battery = ps.sensors_battery()
+    battery_percentage = battery.percent
+    AC_power = battery.power_plugged
+    #Check if the battery is below or equal to 45% and the PC is not in charge
+    if (battery_percentage <= 45) and (AC_power == False) and (loopToken == True):
+            toast(title='Battery about %s%%' % (battery_percentage),
+            body='Battery low, please put in charge your PC',icon=low_battery_icon)
+            loopToken = False #In this way it doesn't go in a infinite loop
+    if (battery_percentage >= 90) and (AC_power == True) and (loopToken == False):
+            toast(title='Battery about %s%%' % (battery_percentage),
+            body='Battery charged, please remove the power cable', 
+            icon=charged_battery_icon)
+            loopToken = True
